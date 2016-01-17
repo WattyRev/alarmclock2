f84f76bbf0f5419d6b89c0ac08f1a34ce6677677
Spencer Watson <spencer@wattydev.com>
2016-01-17 13:13:30 -0800

    toggle enable, and delete alarms

--- alarmClock/app/components/alarm-item.js
+++ alarmClock/app/components/alarm-item.js
@@ -1,5 +1,55 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    tagName: 'li'
+    tagName: 'li',
+    classNames: ['alarm-item'],
+    alarmsService: Ember.inject.service('alarms'),
+
+    displayHours: function() {
+        console.log('calc hours');
+        let hours = this.get('alarm.hours');
+        if (hours > 12) {
+            hours = hours - 12;
+        }
+        if (hours === 0) {
+            hours = 12;
+        }
+        return hours;
+    }.property('alarm.hours'),
+
+    displayMinutes: function() {
+        let minutes = this.get('alarm.minutes');
+        if (minutes < 10) {
+            minutes = '0' + minutes;
+        }
+        return minutes;
+    }.property('alarm.minutes'),
+
+    ampm: function() {
+        let hours = this.get('alarm.hours');
+        let ampm = 'AM';
+        if (hours > 11) {
+            ampm = 'PM';
+        }
+        return ampm;
+    }.property('alarm.hours'),
+
+    actions: {
+        toggleEnabled() {
+            this.set('alarm.isEnabled', !this.get('alarm.isEnabled'));
+        },
+        delete() {
+            let self = this;
+            if (self.get('isDeleting')) {
+                Ember.run.cancel(self.get('deleteTimer'));
+                let index = self.$().index();
+                self.sendAction('deleteAlarm', index);
+            } else {
+                self.set('isDeleting', true);
+                self.set('deleteTimer', Ember.run.later(function() {
+                    self.set('isDeleting', false);
+                }, 500));
+            }
+        }
+    }
 });
--- alarmClock/app/routes/alarms.js
+++ alarmClock/app/routes/alarms.js
@@ -4,5 +4,12 @@ export default Ember.Route.extend({
     alarms: Ember.inject.service('alarms'),
     model() {
         return this.get('alarms.alarms');
+    },
+    actions: {
+        deleteAlarm(index) {
+            let service = this.get('alarms');
+            service.get('alarms').removeAt(index);
+            service.saveAlarms();
+        }
     }
 });
